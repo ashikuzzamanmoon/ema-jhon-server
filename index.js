48b9be307d22c71824c11e08be7225204e4fbf6b
Ashikuzzaman_Moon <ashikuzzamanmoon31@gmail.com>
2023-05-23 18:17:56 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Jhon is busy shopping')
+})
+
+app.listen(port, ()=> {
+    console.log(`ema jhon server is running on port: ${port}`);
+})
